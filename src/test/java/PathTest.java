1dab8eb91e9cb05717c4739199a6a5a55e5946ad
sv.sokolov <sv.sokolov@gmail.com>
2018-05-12 11:24:30 +0700

    refactoring

--- src/test/java/PathTest.java
+++ src/test/java/PathTest.java
@@ -24,94 +24,98 @@ import static org.apache.tinkerpop.gremlin.process.traversal.Scope.local;
 
 public class PathTest {
 
-    private static final Logger LOG = LoggerFactory.getLogger(PathTest.class);
+	private static final Logger LOG = LoggerFactory.getLogger(PathTest.class);
+	private static final String JDBC_URL = "jdbc:postgresql://localhost:5432/sqlg_test";
+	private static final String JDBC_USERNAME = "svs";
+	private static final String JDBC_PASSWORD = "svs";
 
-    @Test
-    public void pathTestSqlg() {
+	@Test
+	public void pathTestSqlg() {
 
-        Configuration configuration = new BaseConfiguration();
-        configuration.addProperty("jdbc.url", "jdbc:postgresql://localhost:5432/sqlg_test");
-        configuration.addProperty("jdbc.username", "svs");
-        configuration.addProperty("jdbc.password", "svs");
+		Configuration configuration = getSqlgConfiguration();
 
-        Graph graph = SqlgGraph.open(configuration);
-        GraphTraversalSource g = graph.traversal();
+		try (Graph graph = SqlgGraph.open(configuration)) {
+			GraphTraversalSource g = graph.traversal();
 
-        GraphTraversal traversal = getTraversal(g);
+			GraphTraversal traversal = getTraversal(g);
 
-        LOG.info("start traversal, vertex count: {}", g.V().count().next());
+			LOG.info("start traversal, vertex count: {}", g.V().count().next());
 
-        while (traversal.hasNext()) {
-            LOG.info(traversal.next().toString());
-        }
-    }
+			while (traversal.hasNext()) {
+				LOG.info(traversal.next().toString());
+			}
+		} catch (Exception e) {
+			LOG.error(e.getMessage(), e);
+		}
 
-    @Test
-    public void pathTestTinkerGraph() {
+	}
 
-        Configuration config = new BaseConfiguration();
-        config.setProperty("gremlin.graph", "org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph");
+	@Test
+	public void pathTestTinkerGraph() {
 
-        Graph graph = GraphFactory.open(config);
-        GraphTraversalSource g = graph.traversal();
-        loadData(graph);
-        GraphTraversal traversal = getTraversal(g);
+		Configuration config = new BaseConfiguration();
+		config.setProperty("gremlin.graph", "org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph");
 
-        LOG.info("start traversal, vertex count: {}", g.V().count().next());
+		Graph graph = GraphFactory.open(config);
+		GraphTraversalSource g = graph.traversal();
+		loadData(graph);
+		GraphTraversal traversal = getTraversal(g);
 
-        while (traversal.hasNext()) {
-            LOG.info(traversal.next().toString());
-        }
-    }
+		LOG.info("start traversal, vertex count: {}", g.V().count().next());
 
-    public GraphTraversal getTraversal(GraphTraversalSource g) {
-        Function timeAtWarehouse = new Function() {
-            @Override
-            public Long apply(Object o) {
-                Map m = (Map) o;
-                Integer dow = ((Edge) (m.get("prev"))).value("dow");
-                Long readyTime = ((Edge) (m.get("prev"))).value("readyTime");
-                Integer depDow = ((Edge) (m.get("curr"))).value("dow");
-                Long depTime = ((Edge) (m.get("curr"))).value("depTime");
-                return (depTime - readyTime) >= 0 ? (depTime - readyTime) : Long.MAX_VALUE;
-            }
-        };
+		while (traversal.hasNext()) {
+			LOG.info(traversal.next().toString());
+		}
+	}
 
-        return g.V().outE("tsw").as("e").inV().emit().repeat(
-                __.flatMap(
-                        __.outE("tsw").filter(__.as("edge").select(last, "e").where(P.eq("edge")).by("speed")).
-                                group().by(__.inV()).by(__.project("curr", "prev").by().by(__.select(last, "e")).fold()).
-                                select(Column.values).unfold().order(local).by(timeAtWarehouse).limit(local, 1).select("curr")
-                ).as("e").inV().simplePath()
-        ).times(20).map(__.union((Traversal) __.select(last, "e").by("speed"), (Traversal) __.path()).fold());
-    }
+	private GraphTraversal getTraversal(GraphTraversalSource g) {
+		Function timeAtWarehouse = (Object o) -> {
+			Map m = (Map) o;
+			Long readyTime = ((Edge) (m.get("prev"))).value("readyTime");
+			Long depTime = ((Edge) (m.get("curr"))).value("depTime");
+			return (depTime - readyTime) >= 0 ? (depTime - readyTime) : Long.MAX_VALUE;
+		};
 
-    @Test
-    public void loadDataSqlg() {
-        Configuration configuration = new BaseConfiguration();
-        configuration.addProperty("jdbc.url", "jdbc:postgresql://localhost:5432/sqlg_test");
-        configuration.addProperty("jdbc.username", "svs");
-        configuration.addProperty("jdbc.password", "svs");
+		return g.V().outE("tsw").as("e").inV().emit().repeat(
+				__.flatMap(
+						__.outE("tsw").filter(__.as("edge").select(last, "e").where(P.eq("edge")).by("speed")).
+								group().by(__.inV()).by(__.project("curr", "prev").by().by(__.select(last, "e")).fold()).
+								select(Column.values).unfold().order(local).by(timeAtWarehouse).limit(local, 1).select("curr")
+				).as("e").inV().simplePath()
+		).times(20).map(__.union((Traversal) __.select(last, "e").by("speed"), (Traversal) __.path()).fold());
+	}
 
-        Graph graph = SqlgGraph.open(configuration);
+	@Test
+	public void loadDataSqlg() {
+		Configuration configuration = getSqlgConfiguration();
 
-        graph.tx().open();
-        loadData(graph);
-        graph.tx().commit();
-    }
+		Graph graph = SqlgGraph.open(configuration);
 
-    public void loadData(Graph graph) {
+		graph.tx().open();
+		loadData(graph);
+		graph.tx().commit();
+	}
 
-        Vertex v0 = graph.addVertex("code", "0");
-        Vertex v1 = graph.addVertex("code", "1");
-        Vertex v2 = graph.addVertex("code", "2");
-        Vertex v3 = graph.addVertex("code", "3");
+	private void loadData(Graph graph) {
+
+		Vertex v0 = graph.addVertex("code", "0");
+		Vertex v1 = graph.addVertex("code", "1");
+		Vertex v2 = graph.addVertex("code", "2");
+		Vertex v3 = graph.addVertex("code", "3");
 
 
-        v0.addEdge("tsw", v1, "speed", "1", "readyTime", 10l, "depTime", 5l, "dow", 1);
-        v1.addEdge("tsw", v2, "speed", "1", "readyTime", 15l, "depTime", 9l, "dow", 1); //must be ignored in longest path
-        v1.addEdge("tsw", v2, "speed", "1", "readyTime", 20l, "depTime", 17l, "dow", 1); //must be used in longest path
-        v2.addEdge("tsw", v3, "speed", "1", "readyTime", 30l, "depTime", 25l, "dow", 1);
-        v1.addEdge("tsw", v2, "speed", "2", "readyTime", 28l, "depTime", 23l, "dow", 1); //speed 2
-    }
+		v0.addEdge("tsw", v1, "speed", "1", "readyTime", 10l, "depTime", 5l, "dow", 1);
+		v1.addEdge("tsw", v2, "speed", "1", "readyTime", 15l, "depTime", 9l, "dow", 1); //must be ignored in longest path
+		v1.addEdge("tsw", v2, "speed", "1", "readyTime", 20l, "depTime", 17l, "dow", 1); //must be used in longest path
+		v2.addEdge("tsw", v3, "speed", "1", "readyTime", 30l, "depTime", 25l, "dow", 1);
+		v1.addEdge("tsw", v2, "speed", "2", "readyTime", 28l, "depTime", 23l, "dow", 1); //speed 2
+	}
+
+	private Configuration getSqlgConfiguration(){
+		Configuration configuration = new BaseConfiguration();
+		configuration.addProperty("jdbc.url", JDBC_URL);
+		configuration.addProperty("jdbc.username", JDBC_USERNAME);
+		configuration.addProperty("jdbc.password", JDBC_PASSWORD);
+		return configuration;
+	}
 }
